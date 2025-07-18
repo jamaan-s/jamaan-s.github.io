5ae941d0a2532ba3c61a523eb3ec22f9a958d5c4
jamaan-s <jamaan198@hotmail.com>
2025-07-18 23:39:37 +0300

    update

---
+++ _nuxt/BPVz_4hA.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,a as n,o as r}from"./Cs7ian0k.js";const a={};function s(c,e){return r(),t("div",null,e[0]||(e[0]=[n("h1",null,"About",-1)]))}const u=o(a,[["render",s]]);export{u as default};
